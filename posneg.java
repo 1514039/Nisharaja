8bded11946bf43627a768c61cc29e8d9eaa26435
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-12 10:14:57 +0530

    Update posneg.java

--- posneg.java
+++ posneg.java
@@ -1,17 +1,7 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
+public static void main (String[] args) 
 	{
-		// your code goes here
-		Scanner in=new Scanner(System.in);
-		int a=in.nextInt();
-		if(a>=1 && a<=1000){
-			System.out.println("Positive");
+	if(a>0){
+	System.out.println("Positive");
 		}
 		else if(a==0){
 			System.out.println("Zero");
@@ -20,4 +10,4 @@ class Ideone
 			System.out.println("Negative")
 		}
 	}
-}
+
