29b39c1341fbe5913a0a373c7187f65256840419
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-14 14:14:03 +0530

    Update Alpha.java

--- Alpha.java
+++ Alpha.java
@@ -3,7 +3,7 @@ public class VowelsCons {
 public static void main(String [] args){
 	Scanner in=new Scanner(System.in);
      char ch=in.next().charAt(0);
-	if((ch>=65 && ch<=90)||(ch>=97 && ch<=122){
+	if((ch>=65 && ch<=90)||(ch>=97 && ch<=122)){
 		System.out.println("Alphabet");
 	}
 	else{
