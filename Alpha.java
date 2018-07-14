f7083d2e66073cb3d36f716c512b1d0cc87a734f
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-14 14:12:51 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class VowelsCons {
+public static void main(String [] args){
+	Scanner in=new Scanner(System.in);
+     char ch=in.next().charAt(0);
+	if((ch>=65 && ch<=90)||(ch>=97 && ch<=122){
+		System.out.println("Alphabet");
+	}
+	else{
+		System.out.println("Not");
+	}
+}
+}
