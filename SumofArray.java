3a9ae246693c0fa5fcf2577e78f7afb69289f84f
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-21 10:00:07 +0530

    Update SumofArray.java

--- SumofArray.java
+++ SumofArray.java
@@ -6,9 +6,12 @@ public class SumofArray {
 		int n=in.nextInt();
 		int k=in.nextInt();
 		int a[]=new int[n];
-		for(int i=0;i<n;i++){
+		for(int i=1;i<n;i++){
 			a[i]=in.nextInt();
 		}
-		System.out.println(a[k]);
+		for(int i=1;i<=k;i++){
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
 	}
 }
