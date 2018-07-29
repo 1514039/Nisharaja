7dca55c4867f8215fdedafe580441a6ba4bc79ef
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-29 14:41:34 +0530

    Update and rename MultiplesOf5.java to MultiplesWith5No.java

--- MultiplesWith5No.java
+++ MultiplesWith5No.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class MultiplesOf5 {
+public class MultiplesWith5No {
 public static void main(String[] args){
 	Scanner in=new Scanner(System.in);
 	int a=in.nextInt();
 	int x=0,count=a;
 	for(int i=1;i<=(5*a);i++){
-		if(i%5==0){
+		if(i%a==0){
 			System.out.print(i+" ");
 		}
 	}
