56885cd1690132cd2c4ae9fe0765fd44b4cafe30
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-07-29 15:56:13 +0530

    Create StringCount.java

---
+++ StringCount.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class StringCount {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int i;
+	String str=in.nextLine();
+	String s[]=str.split(" ");
+	int l=s.length;
+	System.out.println(l);
+}
+}
