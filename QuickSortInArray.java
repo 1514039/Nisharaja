19011f040f2cfa015ef597a8547f322f1e885ad3
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-08-01 09:56:03 +0530

    Create QuickSortInArray.java

---
+++ QuickSortInArray.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QuickSortInArray {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=in.nextInt();
+	}
+Arrays.sort(a);
+for(int i=0;i<n;i++){
+System.out.println(a[i]);
+}
+}
+}
