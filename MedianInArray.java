1ab2328626c3cbb671068067d19a5644bf4cc374
R.Nisha <41136589+1514039@users.noreply.github.com>
2018-08-01 09:52:11 +0530

    Create MedianInArray.java

---
+++ MedianInArray.java
@@ -0,0 +1,16 @@
+	import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MedianInArray {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=in.nextInt();
+	}
+Arrays.sort(a);
+System.out.println(a[n/2]);
+}
+}
